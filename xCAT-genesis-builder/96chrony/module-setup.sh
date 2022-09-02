2e9d6d3e9f27842d6f9204f7193ad3a85ff024c7
Simon Fowler <simon.fowler@anu.edu.au>
2022-09-02 19:49:36 +1000

    Add chrony user and group.

--- xCAT-genesis-builder/96chrony/module-setup.sh
+++ xCAT-genesis-builder/96chrony/module-setup.sh
@@ -18,4 +18,8 @@ install() {
     inst "$moddir/chrony.conf" /etc/chrony.conf
     inst "$moddir/chrony.keys" /etc/chrony.keys
     inst_hook initqueue/online 10 "$moddir/xcat-chronyd-start.sh"
+
+    # cp user and password entries over
+    grep '^chrony:' "$dracutsysrootdir"/etc/passwd >> "$initdir/etc/passwd"
+    grep '^chrony:' "$dracutsysrootdir"/etc/group >> "$initdir/etc/group"
 }
