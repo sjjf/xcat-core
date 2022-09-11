c561104132dea304a7e7eeccdb79909be078d85f
Simon Fowler <simon.fowler@anu.edu.au>
2022-09-11 12:49:34 +1000

    Set hostname to something more useful early on.
    If DHCP doesn't give us a hostname (which is normal during discovery) we
    end up with hostname `(none)`, which isn't very informative. Instead, if
    our destiny is discover, then set the hostname to
    `discover-<bootif-mac>` nice and early, so that we can ensure that
    things like rsyslogd pick it up.
    
    In theory this has potential to confuse other parts of dracut, but as
    far as I can tell hostname='(none)' is only checked once, and that's in
    order to avoid using a completely useless value - nothing else seems to
    care.

--- xCAT-genesis-builder/97xcat/xcat-cmdline.sh
+++ xCAT-genesis-builder/97xcat/xcat-cmdline.sh
@@ -2,6 +2,7 @@
 #
 # add a cmdline entry configuring syslog to point to the xCAT server
 
+# shellcheck disable=1091
 type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
 echo "xCAT: processing kernel command line"
 cmdline=$(getcmdline)
@@ -17,8 +18,22 @@ xcat_ip=${xcatd%:*}
 echo "syslog.server=$xcat_ip" >>/etc/cmdline.d/xcat-cmdline.conf
 echo "*.*" >/tmp/syslog.filters
 
+# some fiddling of the hostname, so that we can avoid annoying log messages
+#
+# Note: only do this when destiny is discover, since in any other context we
+# can expect there to be a proper host entry for the node, which will give it
+# a proper name.
+destiny=$(getarg destiny)
+bootif=$(getarg BOOTIF)
+if [ "$destiny" = "discover" ]; then
+        hostname="${bootif/01/discover}"
+        echo "$hostname" > /proc/sys/kernel/hostname
+fi
+
 # this is necessary for dracut to proceed beyond this hook.
+# shellcheck disable=2034
 root='initramfs'
+# shellcheck disable=2034
 rootok=1
 
 echo "xCAT: done parsing cmdline"
